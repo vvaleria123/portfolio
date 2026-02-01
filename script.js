a0af53875eeebe1c37b18b3eb2bd8dbca68d8452
Valeriia <vostrval@fel.cvut.cz>
2026-02-02 01:59:40 +0200

    Initial portfolio website

---
+++ script.js
@@ -0,0 +1,11 @@
+const sections = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+sections.forEach(section => observer.observe(section));
